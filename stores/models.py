5f9a5307ffe35fc42fcc69af37d1403d04b4bc20
Tony Chou <cool21540125@gmail.com>
2018-04-12 23:17:22 +0800

    只把 models.py的程式碼 copy paste

--- stores/models.py
+++ stores/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Stores(models.Model):
+
+    name = models.CharField(max_length=20)
+    notes = models.TextField(blank=True, default='')
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
+
+    store = models.ForeignKey('Stores', related_name='menu_items')
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name

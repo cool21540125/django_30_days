87b30dfc9d8ccf607b1e8b23472dc2ba6a7a9e94
Tony Chou <cool21540125@gmail.com>
2018-04-13 12:20:43 +0800

    part10 - test 讀完了, 但是測試網址有問題!

--- stores/tests.py
+++ stores/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from .models import MenuItem, Store
 
 # Create your tests here.
+class HomeViewTests(TestCase):
+    def test_home_view(self):
+        res = self.client.get('/stores/')
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, 'home.html')
+
+
+class StoreViewTests(TestCase):
+
+    def setUp(self):
+        Store.objects.create(name='KFC', notes='沒有薄皮嫩雞倒一倒算了啦')
+        mcdonalds = Store.objects.create(name='McDonalds')
+        MenuItem.objects.create(store=mcdonalds, name='大麥克餐', price=129)
+
+    def tearDown(self):
+        Store.objects.all().delete()
+
+    def test_list_view(self):
+        res = self.client.get('/stores/')
+        self.assertContains(
+            res, '<a class="navbar-brand" href="/stores/">午餐系統</a>',
+            html=True,
+        )
+        'Todo: Open' GG了~~
+
+        # //TODO @@
+
+        @critical 測試失敗...
+        # self.assertContains(res, '<a href="/stores/1/">KFC</a>', html=True) 
+        # self.assertContains(res, '沒有薄皮嫩雞倒一倒算了啦')  TODO 測試失敗
+
+    
